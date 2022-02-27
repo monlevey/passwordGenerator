e8ca8a3fb9c2d05985820b7e2fa8ab748b93d3c8
Monique Levey <monique.w.91@outlook.com>
2022-02-27 18:30:49 +0800

    commented out JavaScript steps to begin coding. Updated Css color palette

---
+++ script.js
@@ -0,0 +1,60 @@
+
+  
+let generateBtn = document.querySelector("#generate");
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", function (){
+
+
+
+//on button click - series of modals for password criteria
+    // Get the modal
+    // let modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// let btn = document.getElementById("myBtn");
+
+
+
+
+    //first prompt of password criteria is for length of characters 8 - 128
+ window.prompt('How many characters does your password need? \n(Must be between 8 - 128)');
+ return;
+
+// //answer should be
+// let numberCharacters = 8 
+// function checkCharacters() {
+//     window.confirm('Would you like your password ' + numberCharacters + 'characters long?');
+//     console.log('numberCharacters');
+// }
+//  numberCharacters();
+// window.prompt('Would you like your password ' + numberCharacters + 'characters long?');
+
+//window.confirm;
+        //console.log(password.length); (use the .length parameter to specify how long the password needs to be)
+
+
+    //checkbox (to validate) criteria for type of characters: lowercase, uppercase, numeric, and/or special characters
+        //element.render() checkbox        
+    //identify password criteria options
+//console.log characterSpecifications
+
+    //link checkbox selections to each type of criteria
+
+//prompt if none of the criteria is selected then alert "one or more criteria must be selected"
+
+
+//let user select password criteria - use let for object
+
+//array each letter of alphabet so each is indexed
+//array each special character
+//array each number
+
+//if uppercase is selected math.random > 0.5 (give a percentage of the numberCharacters) .toUppercase which will uppercase a random portion of the characters
+//math.random to 
+//run random password generator to match specific criteria
+
+
+// then password is displayed as an alert or in text on the box window
